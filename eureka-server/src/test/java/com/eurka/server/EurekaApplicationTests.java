01820564483ef1f6dcb4ac0656c05b19db75818f
Soufiane ELAMMARI <SoufianeELAMMARI@cpm.co.ma>
2020-04-12 17:43:04 +0100

    server/client Discovery config

---
+++ eureka-server/src/test/java/com/eurka/server/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
